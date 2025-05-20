eb8efd305efef4c4c9f74214f28dfd107c63f1d3
Hongseongyun <syun4224@naver.com>
2025-05-20 16:50:52 +0900

    05201650

--- APPS/dotdev/src/components/home/Features.jsx
+++ APPS/dotdev/src/components/home/Features.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { features } from '../../constants/data';
+import SectionTitle from '../common/SectionTitle';
 
 const Features = () => {
   return (
@@ -8,23 +9,20 @@ const Features = () => {
         <span className="text-indigo-500 py-1 px-2 text-sm font-medium uppercase rounded-full border border-neutral-500 bg-white dark:bg-neutral-800 dark:border-0">
           Features
         </span>
-        <h2 className="text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20 tracking-wide">
-          Easily Build{' '}
-          <span className=" bg-gradient-to-r from-indigo-300 to-indigo-800 text-transparent bg-clip-text">
-            Your Codes
-          </span>
-        </h2>
+        <SectionTitle whiteString="Easily Build" indigoString="Your Codes" />
       </div>
       <div className="features-cards flex flex-wrap mt-10 lg:mt-20">
         {features.map((feature, idx) => (
           <div key={idx} className="w-full sm:w-1/2 lg:w-1/3">
             <div className="flex mt-10">
-              <div>
-                <div className="mr-2">{feature.icon}</div>
+              <div className="w-10 h-10 bg-white dark:bg-neutral-800 text-indigo-700 p-2 mx-6 rounded-full border-neutral-500 dark:border-0">
+                <div>{feature.icon}</div>
               </div>
               <div>
-                <h5>{feature.text}</h5>
-                <p>{feature.description}</p>
+                <h5 className="mt-1 mb-6 text-xl">{feature.text}</h5>
+                <p className="text-md p-2 mb-20 text-neutral-500">
+                  {feature.description}
+                </p>
               </div>
             </div>
           </div>
