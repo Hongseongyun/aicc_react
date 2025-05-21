932b5ffc3a6d00df94bafb9d3893dc897ec2c55d
Hongseongyun <syun4224@naver.com>
2025-05-21 10:47:28 +0900

    05211047

---
+++ redux/src/redux/store.js
@@ -0,0 +1,16 @@
+import { createStore } from 'redux';
+
+const reducer = (currentState, action) => {
+  if (currentState === undefined) {
+    return {
+      number: 1, // 초기값
+    };
+  }
+  const newState = { ...currentState }; // 새로운 상태를 생성할 때 기존 상태를 유지하여 누적
+  if (action.type === 'PLUS') {
+    newState.number++;
+  }
+  return newState;
+}; // 첫번째 인자는 초기값, 두번째 인자는 변경할 데이터 액션
+
+export const store = createStore(reducer);
